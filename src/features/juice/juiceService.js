6919d00e19d389e45b5f561131ed441a49ce00b3
abhishek sharma <abhi365ddd@gmail.com>
2024-02-27 16:57:05 +0530

    fixed

--- src/features/juice/juiceService.js
+++ src/features/juice/juiceService.js
@@ -1,11 +1,13 @@
+const api_url = '/juice'
+
 const getjuice = async () => {
-  const response = await fetch("http://localhost:8000/juice");
+  const response = await fetch(api_url);
   const data = await response.json();
   return data;
 };
 
 const getsinglejuice = async (id) => {
-  const response = await fetch("http://localhost:8000/juice/" + id);
+  const response = await fetch(api_url + id);
   const data = await response.json();
   return data;
 };
