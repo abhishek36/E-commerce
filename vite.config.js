0cdc10c37fb46ba360cb8bead126b83dcff18464
abhishek sharma <abhi365ddd@gmail.com>
2024-02-27 16:52:44 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  proxy :{
+  '/juice' : {
+    target : "http://localhost:8000/juice",
+    changeOrigin : true
+  }
+  }, 
   plugins: [react()],
 })
